ece7ea810d734c9bcf660dffb226847fd9231232
Ken White <whiteken2002@gmail.com>
2023-04-11 13:18:14 +0300

    Initialized prisma and mongodb and setup route pages and profile screen

---
+++ pages/auth.tsx
@@ -0,0 +1,136 @@
+/* eslint-disable @next/next/no-img-element */
+
+import axios from 'axios';
+import Input from '@/components/Input';
+import { signIn } from 'next-auth/react';
+import React, { useCallback, useState } from 'react';
+
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+
+const Auth = () => {
+  const [username, setUsername] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const [variant, setVariant] = useState<string>('login');
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === 'login' ? 'register' : 'login'
+    );
+  }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        callbackUrl: '/profiles',
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        name: username,
+        password,
+      });
+
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, username, password, login]);
+
+  return (
+    <div className="relative w-full h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-cover bg-fixed">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="Logo" className="h-12" />
+        </nav>
+
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === 'login' ? 'Sign in' : 'Register'}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === 'register' && (
+                <Input
+                  id="username"
+                  type="text"
+                  label="Username"
+                  value={username}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setUsername(e.target.value)
+                  }
+                />
+              )}
+
+              <Input
+                id="email"
+                type="email"
+                label="Email"
+                value={email}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setEmail(e.target.value)
+                }
+              />
+
+              <Input
+                id="password"
+                type="password"
+                label="Password"
+                value={password}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPassword(e.target.value)
+                }
+              />
+            </div>
+
+            <button
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700"
+              onClick={variant === 'login' ? login : register}
+            >
+              {variant === 'login' ? 'Login' : 'Sign up'}
+            </button>
+
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                onClick={() => signIn('google', { callbackUrl: '/profiles' })}
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                onClick={() => signIn('github', { callbackUrl: '/profiles' })}
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
+
+            <p className="text-neutral-500 mt-12">
+              {variant === 'login'
+                ? 'New to Netflix?'
+                : 'Already have an account?'}
+              <span
+                className="text-white ml-1 hover:underline cursor-pointer"
+                onClick={toggleVariant}
+              >
+                {variant === 'login' ? 'Sign up now' : 'Login'}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
