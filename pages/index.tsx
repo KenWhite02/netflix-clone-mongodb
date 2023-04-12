5d975d79092250ef4439404b2d28451f25cc7970
Ken White <whiteken2002@gmail.com>
2023-04-12 12:45:58 +0300

    Implemented billboard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPageContext } from 'next';
 import Navbar from '@/components/Navbar';
 import { getSession } from 'next-auth/react';
+import Billboard from '@/components/Billboard';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,6 +24,7 @@ const Home = () => {
   return (
     <>
       <Navbar />
+      <Billboard />
     </>
   );
 };
